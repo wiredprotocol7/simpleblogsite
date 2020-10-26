b2b9299283587827af55e6bec3350011f1cd02bb
Shubhashish Saha <shubhashish771@outlook.com>
2020-10-26 18:55:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# simpleblogsite
+ 
---
+++ blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    body = models.TextField()
+    objects = models.Manager()
+    published = PublishedManager()
+
+
+class Meta:
+    ordering = ('-publish',)
+
+
+def _str_(self):
+    return self.title
